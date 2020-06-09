fd0be07f26f35c415423b3118b0bb178de93a8e4
bakdoolotkeldibekova <bakdoolotkeldibekova@gmail.com>
2020-06-09 11:50:09 +0600

    create project

---
+++ online-book-store/src/main/java/kg/online/book/store/OnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package kg.online.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+}
---
+++ online-book-store/src/test/java/kg/online/book/store/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.online.book.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
