54acb369d1dfe5fe0b866d5914039ade53771eff
bakdoolotkeldibekova <bakdoolotkeldibekova@gmail.com>
2020-07-08 14:23:56 +0600

    probllem with database

--- online-book-store/src/main/java/kg/online/book/store/repository/ProductRepository.java
+++ online-book-store/src/main/java/kg/online/book/store/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package kg.online.book.store.repository;
 
-import kg.online.book.store.entity.Genre;
 import kg.online.book.store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
